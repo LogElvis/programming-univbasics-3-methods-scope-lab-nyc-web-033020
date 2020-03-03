b6541cc38bdb2ff5a7c224d41375d8679621a406
Elvis Ning <ningelvis@gmail.com>
2020-03-03 23:17:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,3 +10,9 @@ end
 def link
   puts phrase = "It's Dangerous To Go Alone! Take This."
 end
+
+def all_phrases
+  puts phrase = "It's-a me, Mario!"
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
+end  
